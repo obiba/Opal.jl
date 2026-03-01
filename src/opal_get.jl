c4168bf18afa1b3a11d1800a42a73fbfa6374d50
Hugo Solleder <hugo.solleder@epfl.ch>
2026-03-01 13:15:49 +0100

    fix: cleanup opal_get

--- src/opal_get.jl
+++ src/opal_get.jl
@@ -1,35 +1,43 @@
+# TODO: investigate if we can use the terminate_on functionality in Julia.
+# This would rely on passing a function to the retry_check kwarg
 function opal_get(
     opal::OpalObject,
     args...;
-    query::Dict{String,Any}=Dict(),
+    query::Dict{String,Any}=Dict{String,Any}(),
     acceptType::String="application/json",
     outFile::Union{String,Nothing}=nothing,
     callback::Union{Function,Nothing}=nothing,
+    retries::Int=3,
 )
     r = nothing
     logerrors = false
-    times = 3
+    headers = Dict("Accept" => acceptType)
+
     url = _url(opal, args...)
-    if isnothing(outFile)
-        r = request(
+    r = if isnothing(outFile)
+        request(
             "GET",
             url;
             query=query,
-            headers=Dict("Accept" => acceptType),
+            headers=headers,
             logerrors=logerrors,
-            times=times,
+            retry=true,
+            retries=retries,
         )
     else
-        r = request(
-            "GET",
-            url;
-            query=query,
-            headers=Dict("Accept" => acceptType),
-            logerrors=logerrors,
-            times=times,
-            # write_disk(outFile, overwrite = true),
-            # config = opal.config,
-        )
+        throw(ErrorException("Downloading to file is not supported for GET requests."))
+        open(outFile, "w") do io
+            HTTP.request(
+                "GET",
+                url;
+                query=query,
+                headers=headers,
+                response_stream=io,
+                logerrors=logerrors,
+                retry=true,
+                retries=retries,
+            )
+        end
     end
 
     _handleResponseOrCallback(opal, r, callback)
