a5ba489aba8b89a2a244cca57d0d14f6fb0676b3
Hugo Solleder <hsolleder@users.noreply.github.com>
2026-04-03 14:37:29 +0200

    refactor: relax String parameter for Opal resource functions

--- src/opal_resource.jl
+++ src/opal_resource.jl
@@ -5,10 +5,10 @@ Get the resource references of a project.
 
 # Arguments
 - `opal::OpalObject`: Opal connection object
-- `project::String`: Name of the project
+- `project::AbstractString`: Name of the project
 - `df::Bool=true`: Return a DataFrame (not implemented, returns raw response)
 """
-function opal_resources(opal::OpalObject, project::String; df::Bool=true)
+function opal_resources(opal::OpalObject, project::AbstractString; df::Bool=true)
     if !ismissing(opal.version) && opal.version < v"3.0"
         throw(
             ErrorException(
@@ -43,10 +43,10 @@ Get a resource reference of a project.
 
 # Arguments
 - `opal::OpalObject`: Opal connection object
-- `project::String`: Name of the project
-- `resource::String`: Name of the resource in the project
+- `project::AbstractString`: Name of the project
+- `resource::AbstractString`: Name of the resource in the project
 """
-function opal_resource(opal::OpalObject, project::String, resource::String)
+function opal_resource(opal::OpalObject, project::AbstractString, resource::AbstractString)
     opal_get(opal, "project", project, "resource", resource)
 end
 
@@ -57,10 +57,12 @@ Check whether a resource reference exists in a project (and is visible by the re
 
 # Arguments
 - `opal::OpalObject`: Opal connection object
-- `project::String`: Name of the project
-- `resource::String`: Name of the resource in the project
+- `project::AbstractString`: Name of the project
+- `resource::AbstractString`: Name of the resource in the project
 """
-function opal_resource_exists(opal::OpalObject, project::String, resource::String)
+function opal_resource_exists(
+    opal::OpalObject, project::AbstractString, resource::AbstractString
+)
     res = try
         opal_resource(opal, project, resource)
     catch
@@ -78,10 +80,12 @@ Note: This is a placeholder implementation. The original R implementation uses s
 
 # Arguments
 - `opal::OpalObject`: Opal connection object
-- `project::String`: Name of the project
-- `resource::String`: Name of the resource in the project
+- `project::AbstractString`: Name of the project
+- `resource::AbstractString`: Name of the resource in the project
 """
-function opal_resource_get(opal::OpalObject, project::String, resource::String)
+function opal_resource_get(
+    opal::OpalObject, project::AbstractString, resource::AbstractString
+)
     throw(
         ErrorException(
             "opal_resource_get is not yet fully implemented. This function requires server-side R session management (opal.assign.resource, opal.execute, opal.symbol_rm) which is not yet available.",
@@ -101,25 +105,25 @@ Create a resource reference in a project.
 
 # Arguments
 - `opal::OpalObject`: Opal connection object
-- `project::String`: Name of the project
-- `name::String`: Name of the resource in the project
-- `url::String`: The URL of the resource
-- `description::Union{String,Nothing}=nothing`: The description of the resource
-- `format::Union{String,Nothing}=nothing`: The format of the data described by the resource
-- `package::Union{String,Nothing}=nothing`: The R package to be loaded prior to the assignment of the resource
-- `identity::Union{String,Nothing}=nothing`: The identity key or username to be used when accessing the resource
-- `secret::Union{String,Nothing}=nothing`: The secret key or password to be used when accessing the resource
+- `project::AbstractString`: Name of the project
+- `name::AbstractString`: Name of the resource in the project
+- `url::AbstractString`: The URL of the resource
+- `description::Union{AbstractString,Nothing}=nothing`: The description of the resource
+- `format::Union{AbstractString,Nothing}=nothing`: The format of the data described by the resource
+- `package::Union{AbstractString,Nothing}=nothing`: The R package to be loaded prior to the assignment of the resource
+- `identity::Union{AbstractString,Nothing}=nothing`: The identity key or username to be used when accessing the resource
+- `secret::Union{AbstractString,Nothing}=nothing`: The secret key or password to be used when accessing the resource
 """
 function opal_resource_create(
     opal::OpalObject,
-    project::String,
-    name::String,
-    url::String;
-    description::Union{String,Nothing}=nothing,
-    format::Union{String,Nothing}=nothing,
-    package::Union{String,Nothing}=nothing,
-    identity::Union{String,Nothing}=nothing,
-    secret::Union{String,Nothing}=nothing,
+    project::AbstractString,
+    name::AbstractString,
+    url::AbstractString;
+    description::Union{AbstractString,Nothing}=nothing,
+    format::Union{AbstractString,Nothing}=nothing,
+    package::Union{AbstractString,Nothing}=nothing,
+    identity::Union{AbstractString,Nothing}=nothing,
+    secret::Union{AbstractString,Nothing}=nothing,
 )
     parameters = Dict{String,Any}("url" => url)
     if !isnothing(format)
@@ -160,23 +164,23 @@ Create an extended resource reference in a project.
 
 # Arguments
 - `opal::OpalObject`: Opal connection object
-- `project::String`: Name of the project
-- `name::String`: Name of the resource in the project
-- `provider::String`: Name of the R package in which the resource is defined
-- `factory::String`: Name of the JS function that turns parameters and credentials into a resource object
-- `parameters::Dict{String,Any}`: A dictionary of the resource parameters
-- `description::Union{String,Nothing}=nothing`: The description of the resource
-- `credentials::Union{Dict{String,Any},Nothing}=nothing`: A dictionary of the resource credentials
+- `project::AbstractString`: Name of the project
+- `name::AbstractString`: Name of the resource in the project
+- `provider::AbstractString`: Name of the R package in which the resource is defined
+- `factory::AbstractString`: Name of the JS function that turns parameters and credentials into a resource object
+- `parameters::Dict{AbstractString,Any}`: A dictionary of the resource parameters
+- `description::Union{AbstractString,Nothing}=nothing`: The description of the resource
+- `credentials::Union{Dict{AbstractString,Any},Nothing}=nothing`: A dictionary of the resource credentials
 """
 function opal_resource_extension_create(
     opal::OpalObject,
-    project::String,
-    name::String,
-    provider::String,
-    factory::String,
-    parameters::Dict{String,Any};
-    description::Union{String,Nothing}=nothing,
-    credentials::Union{Dict{String,Any},Nothing}=nothing,
+    project::AbstractString,
+    name::AbstractString,
+    provider::AbstractString,
+    factory::AbstractString,
+    parameters::Dict{AbstractString,Any};
+    description::Union{AbstractString,Nothing}=nothing,
+    credentials::Union{Dict{AbstractString,Any},Nothing}=nothing,
 )
     if !opal_resource_exists(opal, project, name)
         resjson = Dict{String,Any}(
@@ -212,12 +216,12 @@ Delete a resource reference. Removes the reference to a resource. The targeted r
 
 # Arguments
 - `opal::OpalObject`: Opal connection object
-- `project::String`: Project name where the resource is located
-- `resource::String`: Resource name to be deleted
+- `project::AbstractString`: Project name where the resource is located
+- `resource::AbstractString`: Resource name to be deleted
 - `silent::Bool=true`: Warn if resource does not exist
 """
 function opal_resource_delete(
-    opal::OpalObject, project::String, resource::String; silent::Bool=true
+    opal::OpalObject, project::AbstractString, resource::AbstractString; silent::Bool=true
 )
     if opal_resource_exists(opal, project, resource)
         opal_delete(opal, "project", project, "resource", resource)
